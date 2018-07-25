2a3d842e6be93d22ecf6c75948283887a47338b7
Marco <marcovoort@gmail.com>
2018-07-25 14:09:05 +0200

    Prevent syntax error in SQL for empty forms

--- src/Storage/PDO_SQLite_Storage.php
+++ src/Storage/PDO_SQLite_Storage.php
@@ -152,11 +152,13 @@ class PDO_SQLite_Storage extends PDOStorage {
 			throw new \Exception("Could not create table __tmp__migration.");
 		}
 		
-		$sth = $this->PDO->prepare("INSERT INTO __tmp__migration (".implode(', ', $a_columnsNew).") SELECT ".implode(', ', $a_columnsOld)." FROM ".$this->es_table." ");
-		$sth->execute();
-		
-		if($sth->errorCode() !== '00000') {
-			throw new \Exception("Could not fill table __tmp__migration.");
+		if(count($a_columnsOld) > 0) {
+			$sth = $this->PDO->prepare("INSERT INTO __tmp__migration (".implode(', ', $a_columnsNew).") SELECT ".implode(', ', $a_columnsOld)." FROM ".$this->es_table." ");
+			$sth->execute();
+			
+			if($sth->errorCode() !== '00000') {
+				throw new \Exception("Could not fill table __tmp__migration.");
+			}
 		}
 		
 		$sth = $this->PDO->prepare("DROP TABLE ".$this->es_table." ");
