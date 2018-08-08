28e2caf66f2ca3bba131feba66b02d8936149090
Marco <marcovoort@gmail.com>
2018-08-08 14:14:09 +0200

    Also check for ValidationException when building TempForm forms

--- src/TempForm.php
+++ src/TempForm.php
@@ -9,6 +9,11 @@ class TempForm extends Form {
 	}
 	
 	public function checkUpdateDataLoss(array $a_definition, array $a_fieldRenames = []) {
+		// Temp forms have no stored submissions, so also no data loss
+		// We do import the definition here, to check for any ValidationException
+		$Form2 = clone $this;
+		$Form2->importDefinition($a_definition);
+		
 		return [];
 	}
 	
