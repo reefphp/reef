b0e0b4b3524fb406eecb75e21d4d4ced055e7048
Marco <marcovoort@gmail.com>
2018-08-24 09:24:46 +0200

    Make Select conditionable

--- src/Components/Select/SelectValue.php
+++ src/Components/Select/SelectValue.php
@@ -6,4 +6,47 @@ use Reef\Components\AbstractSingleChoice\AbstractSingleChoiceValue;
 
 class SelectValue extends AbstractSingleChoiceValue {
 	
+	
+	/**
+	 * @inherit
+	 */
+	public function validateConditionOperation(string $s_operator, $m_operand) {
+		if(in_array($s_operator, ['is empty', 'is not empty'])) {
+			if(!empty($m_operand)) {
+				throw new \Reef\Exception\ConditionException('Empty does not take an operand');
+			}
+		}
+		
+		if(in_array($s_operator, ['equals', 'does not equal'])) {
+			$b_found = false;
+			foreach($this->getField()->getDeclaration()['options'] as $a_option) {
+				if($a_option['name'] === $m_operand) {
+					$b_found = true;
+					break;
+				}
+			}
+			if(!$b_found) {
+				throw new \Reef\Exception\ConditionException('Invalid operand "'.$m_operand.'"');
+			}
+		}
+	}
+	
+	/**
+	 * @inherit
+	 */
+	public function evaluateConditionOperation(string $s_operator, $m_operand) : bool {
+		switch($s_operator) {
+			case 'equals':
+				return $this->s_value == $m_operand;
+				
+			case 'does not equal':
+				return $this->s_value != $m_operand;
+				
+			case 'is empty':
+				return $this->s_value == '';
+				
+			case 'is not empty':
+				return $this->s_value != '';
+		}
+	}
 }
--- src/Components/Select/script.js
+++ src/Components/Select/script.js
@@ -47,5 +47,71 @@ Reef.addComponent((function() {
 		}
 	};
 	
+	Field.getConditionOperators = function() {
+		return [
+			'equals',
+			'does not equal',
+			'is empty',
+			'is not empty',
+		];
+	};
+	
+	Field.prototype.getConditionOperandInput = function(operator, layout) {
+		var self = this;
+		
+		var classes = '';
+		if(layout == 'bootstrap4') {
+			classes += ' form-control';
+		}
+		
+		if(operator.indexOf('equal') > -1) {
+			var $select = $('<select class="'+classes+'">');
+			
+			this.$field.find('select option').each(function() {
+				$select.append($('<option>').val($(this).attr('value')).text($(this).text()));
+			});
+			
+			return $select;
+		}
+		
+		return null;
+	};
+	
+	Field.prototype.validateConditionOperation = function(operator, operand) {
+		if(['is empty', 'is not empty'].indexOf(operator) > -1) {
+			if(operand != '') {
+				throw 'Empty does not take an operand';
+			}
+		}
+		
+		if(['equals', 'does not equal'].indexOf(operator) > -1) {
+			var found = false;
+			this.$field.find('select option').each(function() {
+				if($(this).attr('value') === operand) {
+					found = true;
+					return false;
+				}
+			});
+			if(!found) {
+				throw ('Invalid operand "'+operand+'"');
+			}
+		}
+	};
+	
+	Field.prototype.evaluateConditionOperation = function(operator, operand) {
+		var value = this.getValue();
+		
+		switch(operator) {
+			case 'equals':
+				return value == operand;
+			case 'does not equal':
+				return value != operand;
+			case 'is empty':
+				return value == '';
+			case 'is not empty':
+				return value != '';
+		};
+	};
+	
 	return Field;
 })());
